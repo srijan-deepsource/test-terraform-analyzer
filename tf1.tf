2f48dc10227da41df2346f5b06f8551fce2138f7
Srijan Saurav <srijan@deepsource.io>
2020-11-20 14:12:34 +0530

    Test files

---
+++ tf1.tf
@@ -0,0 +1,15 @@
+resource "aws_db_instance" "mysql" {
+  identifier             = "app"
+  allocated_storage      = 50
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "5.7.11"
+  instance_class         = "db.m4.large"
+  name                   = "app_db"
+  port                   = 3306
+  publicly_accessible    = false
+  vpc_security_group_ids = ["${aws_security_group.mysql.id}"]
+  db_subnet_group_name   = "app-subnet-group"
+  parameter_group_name   = "default.mysql5.7" // default DB parameter group!
+  multi_az               = true
+}
---
+++ tf2.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.6.17"
+  instance_class       = "db.t1.micro" // previous generation instance type!
+  name                 = "mydb"
+  username             = "foo"
+  password             = "bar"
+  db_subnet_group_name = "my_database_subnet_group"
+  parameter_group_name = "default.mysql5.6"
+}
